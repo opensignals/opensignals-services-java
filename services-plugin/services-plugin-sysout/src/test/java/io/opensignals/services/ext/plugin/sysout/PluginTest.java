22bb4accd08fcbef145d3a9c35d36e9fb6847547
William Louth <william@autoletics.com>
2021-05-16 17:56:31 +0200

    project dependency updates

--- services-plugin/services-plugin-sysout/src/test/java/io/opensignals/services/ext/plugin/sysout/PluginTest.java
+++ services-plugin/services-plugin-sysout/src/test/java/io/opensignals/services/ext/plugin/sysout/PluginTest.java
@@ -27,6 +27,7 @@ import java.util.Properties;
 import static io.opensignals.services.Services.*;
 import static io.opensignals.services.Services.Orientation.EMIT;
 import static io.opensignals.services.Services.Signal.SUCCEED;
+import static io.opensignals.services.Services.Status.OK;
 import static java.lang.System.setOut;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -99,14 +100,14 @@ final class PluginTest {
         EMIT,
         SUCCEED
       );
-/*
+
     verify ( OUT )
       .printf (
         "%s [%s] %s%n",
         name,
         EMIT,
         OK
-      );*/
+      );
 
   }
 
